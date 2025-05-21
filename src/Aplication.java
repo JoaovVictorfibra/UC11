aae0d0b75beba9159016eb12955052d36634dfb8
Joao Victor <joaovvitor324@gmail.com>
2025-05-20 23:45:02 -0300

    Foi criado um nova classe chamada de Aplication que define a tela de cadastro como tela inicial do nosso projeto

---
+++ src/Aplication.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author joaov
+ */
+public class Aplication {
+    public static void main(String[] args) {
+        cadastroVIEW inicial = new cadastroVIEW();
+        inicial.setVisible(true);
+    }
+}
